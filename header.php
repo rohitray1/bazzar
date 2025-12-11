46192c8265bdfa7a0446475a7a58190bf9fbb34e
Manish Sahu <manishnfriends@gmail.com>
2025-12-11 17:11:36 +0545

    code for acf and bootstrap

--- header.php
+++ header.php
@@ -4,7 +4,10 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <?php wp_head(); ?>   <!-- ✅ THIS LINE IS MOST IMPORTANT -->
 </head>
 <body <?php body_class(); ?>>
--- single.php
+++ single.php
@@ -1,45 +1,43 @@
 <?php
 get_header();
 ?>
+
 <section class="clothes-section">
     <?php 
     if ( have_posts() ) :
     while ( have_posts() ) : the_post();
-        ?>
-    <div class="clothes-container">
+    ?>
+    <div class="row g-4 align-items-start">
 
-        <div class="">
+      <!-- Left Column: PRODUCT IMAGE -->
+      <div class="col-md-6">
+        
+        <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-thumbnail rounded shadow-sm" alt="Product Image">
+      </div>
 
-           
+      <!-- Right Column: PRODUCT DETAILS -->
+      <div class="col-md-6">
+        
+        <h2 class="mb-3"><?php the_title(); ?></h2>
 
-            <h3><?php the_title(); ?></h3>
+        <p class="text-muted">
+         <?php the_content();?>
+        </p>
 
-          <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" />
-       
-            <a href="<?php echo esc_url( get_permalink() ); ?>" class="buy-btn">
-                View Product
-            </a>
+        <h4 class="text-primary mb-3">Rs <?php the_field('price');?></h4>
+        <h5 class="text-primary mb-3">Stock <?php the_field('stock');?></h5>
 
+        <button class="btn btn-primary btn-lg">Add to Cart</button>
         </div>
-<div class="">
 
-           
-
-            <h3><?php the_title(); ?></h3>
-
-          
-        <p><php the_content(); ?></p>
-            
-
-        </div>
-            <?php
+    </div>
+    <?php
     endwhile;
 else :
     echo '<p>No content found</p>';
 endif;
 ?>
 
-    </div>
 </section>
 
 <?php
